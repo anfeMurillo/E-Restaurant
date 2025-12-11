a98cf7fb47ac20591ecb2116311c248ae0247ffb
anfeMurillo <anfehumu2.0@hotmail.com>
2025-12-10 19:23:20 -0500

    feat:Crear contrato order item.

---
+++ src/contracts/orderItem/orderItem.go
@@ -0,0 +1,18 @@
+package orderitem
+
+import (
+	"context"
+	orderitem "e-restaurant/models/orderItem"
+)
+
+type Repository interface {
+	Create(ctx context.Context, oItem *orderitem.OrderItem) (*orderitem.OrderItem, error)
+
+	GetById(ctx context.Context, oItemId int) (*orderitem.OrderItem, error)
+
+	GetAllByOrder(ctx context.Context, orderId int) ([]orderitem.OrderItem, error)
+
+	Delete(ctx context.Context, oItemId int)
+
+	UpdateQuantity(ctx context.Context, oItemId int, quantity int)
+}
